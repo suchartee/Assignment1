fe205bc2760ca9b6d44d85177371966dd3121add
Suchartee Kitisopakul (Alice) <suchartee.k@gmail.com>
2017-07-21 13:45:34 -0700

    Update serial_test.cpp

--- serial_test.cpp
+++ serial_test.cpp
@@ -7,97 +7,92 @@
 #include <vector>
 using namespace std;
 
-/*
-* Forks children and lets them perform their tasks
-* @param urls - the URLs to download
-* @param url_counter - total amount of urls
-*/
-void create_children_parallel(vector<string>& urls, int& url_counter)
-{
+/**
+ * Forks children and lets them perform their tasks
+ * @param urls - the urls to download
+ */
+void create_children_serial(vector<string>& urls)
+{	
 	/* The process id */
 	pid_t pid;
-
+	
 	/* parallel */
 
 	/* serial */
 	/* Go through all t Make sure the fork was a success URLs */
-	for (vector<string>::iterator urlIt = urls.begin(); urlIt != urls.end(); ++urlIt) {
+	for(vector<string>::iterator urlIt = urls.begin(); urlIt != urls.end(); ++urlIt) {
 		/* Create a child */
 		pid = fork();
 
 		/* */
-		if (pid < 0) {
+		if(pid < 0) {
 			perror("fork");
 			exit(1);
 		}
 		/* The child code */
-		if (pid == 0) {
+		
+		if(pid == 0) {			
 			fclose(stdout);
 			fclose(stderr);
 			
+
 			/* Deploy wget */
-			if (execlp("/usr/bin/wget", "child", urlIt->c_str(), NULL) < 0) {
+			if(execlp("/usr/bin/wget", "child", urlIt->c_str(), NULL) < 0) {
 				perror("execlp");
 				exit(1);
 			}
+			
 		}
-	}
+		else {
+			wait(NULL);
+		}
+	}	
 }
 
 
-/*
-* Read the URLs from the file
-* @param urls - the URLs to download
-* @param url_counter - total amount of urls
-*/
+/**
+ * Read the URLs from the file
+ * @param urls - the URLs to download
+ */
 
-void readUrls(vector<string>& urls, int& url_counter) {
+void readUrls(vector<string>& urls) {
 	/* Open the file */
-	ifstream urlFile("urls.txt");
+	ifstream urlFile("urls.txt");		
+	
 	/* The URL buffer */
 	string urlBuffer;
-
-	/* Make sure the file can be opened */
-	if (!urlFile.is_open()) {
+	
+	/* Make sure the file was opened */
+	if(!urlFile.is_open()) {
 		fprintf(stderr, "Failed to open the file");
 		exit(1);
 	}
-
+	
 	/* Read the entire file */
-	while (!urlFile.eof()) {
+	while(!urlFile.eof()) {
 		/* Read the buffer */
 		urlFile >> urlBuffer;
-
-		/* Push each url into urls vector */
-		if (!urlFile.eof()) {
-			urls.push_back(urlBuffer);
-			++url_counter;
-		}
+		
+		/* Are we at the end of the file */
+		if(!urlFile.eof())
+			urls.push_back(urlBuffer);	
 	}
-
+	
 	/* Close the file */
 	urlFile.close();
 }
 
-int main() {
+int main() {		
 	/* Read the URL files */
 	vector<string> urls;
-
-	/* Counter for total amount of URLs */
-	int url_counter = 0;
-
+		
 	/* Read the URLs */
-	readUrls(urls, url_counter);
-
+	readUrls(urls);
+	
 	/* Create child processes */
-	create_children_parallel(urls, url_counter);
+	create_children_serial(urls);
 
-	/* The parent code */
-	/* Parent will wait for all children to terminate */
-	while (url_counter > 0) {
-		wait(NULL);
-		--url_counter;
-	}
-
-	return 0;
+		
+	return 0;	
+	
 }
