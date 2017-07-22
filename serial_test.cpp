554943c8a88e6a6319790daafc5e7d011685208d
Suchartee Kitisopakul (Alice) <suchartee.k@gmail.com>
2017-07-21 20:50:31 -0700

    Update serial_test.cpp

--- serial_test.cpp
+++ serial_test.cpp
@@ -1,100 +1,154 @@
 #include <unistd.h>
+
 #include <sys/types.h>
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <sys/wait.h>
+
 #include <fstream>
+
 #include <vector>
+
 using namespace std;
 
-/**
- * Forks children and lets them perform their tasks
- * @param urls - the urls to download
- */
-void create_children_serial(vector<string>& urls)
-{	
-	/* The process id */
-	pid_t pid;
-	
-	/* parallel */
 
-	/* serial */
+
+/**
+
+* Forks children and lets them perform their tasks
+
+* @param urls - the urls to download
+
+*/
+
+void create_children_serial(vector<string>& urls)
+{
+	/* The process id */
+
+	pid_t pid;
+
+
 	/* Go through all t Make sure the fork was a success URLs */
-	for(vector<string>::iterator urlIt = urls.begin(); urlIt != urls.end(); ++urlIt) {
+
+	for (vector<string>::iterator urlIt = urls.begin(); urlIt != urls.end(); ++urlIt) {
+
 		/* Create a child */
+
 		pid = fork();
 
-		/* */
-		if(pid < 0) {
+		if (pid < 0) {
+
 			perror("fork");
+
 			exit(1);
+
 		}
+
+
 		/* The child code */
-		
-		if(pid == 0) {			
-			fclose(stdout);
-			fclose(stderr);
-			
+
+		else if (pid == 0) {
 
 			/* Deploy wget */
-			if(execlp("/usr/bin/wget", "child", urlIt->c_str(), NULL) < 0) {
+
+			/* Check if execlp run a success */
+			printf("Child is created\n");
+			if (execlp("/usr/bin/wget", "wget", urlIt->c_str(), NULL) < 0) {
+
 				perror("execlp");
+
 				exit(1);
-			} else {
-				execlp("/usr/bin/wget", "child", urlIt->c_str(), NULL);
+
 			}
-			
+
 		}
 		else {
+			/* Parent waits until a child exits */
+			printf("Parent is waiting till a child exits\n");
 			wait(NULL);
 		}
-	}	
+
+	}
+
 }
+
+
 
 
 /**
- * Read the URLs from the file
- * @param urls - the URLs to download
- */
+
+* Read the URLs from the file
+
+* @param urls - the URLs to download
+
+*/
+
 
 void readUrls(vector<string>& urls) {
+
 	/* Open the file */
-	ifstream urlFile("urls.txt");		
-	
+
+	ifstream urlFile("urls.txt");
 	/* The URL buffer */
 	string urlBuffer;
-	
+
+
 	/* Make sure the file was opened */
-	if(!urlFile.is_open()) {
+
+	if (!urlFile.is_open()) {
+
 		fprintf(stderr, "Failed to open the file");
+
 		exit(1);
+
 	}
-	
+
+
 	/* Read the entire file */
-	while(!urlFile.eof()) {
+
+	while (!urlFile.eof()) {
+
 		/* Read the buffer */
+
 		urlFile >> urlBuffer;
-		
+
+
 		/* Are we at the end of the file */
-		if(!urlFile.eof())
-			urls.push_back(urlBuffer);	
+
+		if (!urlFile.eof())
+
+			urls.push_back(urlBuffer);
 	}
-	
+
+
 	/* Close the file */
+
 	urlFile.close();
+
 }
 
-int main() {		
-	/* Read the URL files */
-	vector<string> urls;
-		
-	/* Read the URLs */
-	readUrls(urls);
-	
-	/* Create child processes */
-	create_children_serial(urls);
 
-		
-	return 0;	
-	
+
+int main() {
+
+	/* Read the URL files */
+
+	vector<string> urls;
+
+
+	/* Read the URLs */
+
+	readUrls(urls);
+
+
+	/* Create child processes */
+
+	create_children_serial(urls);
+	printf("All children exited\n");
+
+	return 0;
+
 }
