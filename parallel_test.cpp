80115a1e707091e1bf9f75f8aa1a2a23e05e8679
Suchartee Kitisopakul (Alice) <suchartee.k@gmail.com>
2017-07-25 00:01:10 -0700

    Update parallel_test.cpp

--- parallel_test.cpp
+++ parallel_test.cpp
@@ -1,103 +1,178 @@
+/* 
+* ==============================================================================
+*     Authors:		Suchartee (Alice) Kitisopakul, Su Win Htet, Qiyuan Liu
+*     Date:   		25 July 2017
+*     File name:  	serial.cpp
+*     Description: 	This serial file downloader is a program used for 
+*       		downloading multiple files from the Internet one by one.   
+*       		The program uses fork() and execlp("wget") commands.
+* 			The program reads urls.txt containing url file download 
+*			links as a commandline, and perform the downloading tasks. 
+* ==============================================================================
+*/
 #include <unistd.h>
+
 #include <sys/types.h>
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <sys/wait.h>
+
 #include <fstream>
+
 #include <vector>
+
+
 using namespace std;
 
-/*
+
+
+/* Global variables for link counter */
+int link_counter = 0;
+
+/**
+
 * Forks children and lets them perform their tasks
-* @param urls - the URLs to download
-* @param url_counter - total amount of urls
+
+* @param urls - the urls to download
+
 */
-void create_children_parallel(vector<string>& urls, int& url_counter)
+
+void create_children_serial(vector<string>& urls)
 {
 	/* The process id */
+
 	pid_t pid;
 
-	/* parallel */
 
-	/* serial */
-	/* Go through all t Make sure the fork was a success URLs */
+	/* Make sure the fork was a success URLs */
+
 	for (vector<string>::iterator urlIt = urls.begin(); urlIt != urls.end(); ++urlIt) {
+
 		/* Create a child */
+
 		pid = fork();
 
-		/* */
 		if (pid < 0) {
+
 			perror("fork");
+
 			exit(1);
+
 		}
+
+
 		/* The child code */
-		if (pid == 0) {
-			fclose(stdout);
-			fclose(stderr);
-			
+
+		else if (pid == 0) {
+
 			/* Deploy wget */
-			if (execlp("/usr/bin/wget", "child", urlIt->c_str(), NULL) < 0) {
+
+			printf("Child is created with PID %d, from parent PID %d\n", getpid(), getppid());
+			/* Check if execlp run a success */
+
+			if (execlp("/usr/bin/wget", "wget", urlIt->c_str(), NULL) < 0) {
+
 				perror("execlp");
+
 				exit(1);
+
 			}
+
 		}
+
+		else {
+
+			/* Parent waits until a child exits */
+			wait(NULL);
+
+			++link_counter;
+			printf("File %d is complete!\n", link_counter);
+
+		}
+
 	}
+
 }
 
 
-/*
+
+
+/**
+
 * Read the URLs from the file
+
 * @param urls - the URLs to download
-* @param url_counter - total amount of urls
+
 */
 
-void readUrls(vector<string>& urls, int& url_counter) {
+
+void readUrls(vector<string>& urls) {
+
 	/* Open the file */
+
 	ifstream urlFile("urls.txt");
 	/* The URL buffer */
 	string urlBuffer;
 
-	/* Make sure the file can be opened */
+
+	/* Make sure the file was opened */
+
 	if (!urlFile.is_open()) {
+
 		fprintf(stderr, "Failed to open the file");
+
 		exit(1);
+
 	}
+
 
 	/* Read the entire file */
+
 	while (!urlFile.eof()) {
+
 		/* Read the buffer */
+
 		urlFile >> urlBuffer;
 
-		/* Push each url into urls vector */
-		if (!urlFile.eof()) {
-			urls.push_back(urlBuffer);
-			++url_counter;
-		}
+
+		/* Push each url into the vector */
+
+		urls.push_back(urlBuffer);
 	}
+
 
 	/* Close the file */
+
 	urlFile.close();
+
 }
 
+
+
 int main() {
+
 	/* Read the URL files */
+
 	vector<string> urls;
 
-	/* Counter for total amount of URLs */
-	int url_counter = 0;
 
 	/* Read the URLs */
-	readUrls(urls, url_counter);
 
+	readUrls(urls);
+
+
+	printf("Getting urls\n");
+	printf("There are %d links in urls.txt\n", urls.size());
 	/* Create child processes */
-	create_children_parallel(urls, url_counter);
 
-	/* The parent code */
-	/* Parent will wait for all children to terminate */
-	while (url_counter > 0) {
-		wait(NULL);
-		--url_counter;
-	}
+	create_children_serial(urls);
+
+	/* Successfully downloading */
+	printf("Downloading is done\n");
 
 	return 0;
+
 }
